e716678425c984791e7c278ec8024d1dc777941b
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2023-10-07 10:30:56 +0200

    feat(pkg-management): expose CLI `create-pnpm-patch-via-ts-patch` as `bin`

---
+++ .changeset/green-moons-pretend.md
@@ -0,0 +1,5 @@
+---
+'@pkerschbaum/pkg-management': minor
+---
+
+expose CLI `create-pnpm-patch-via-ts-patch` as `bin`
--- packages/pkg-management/src/create-pnpm-patch-via-ts-patch.mjs
+++ packages/pkg-management/src/create-pnpm-patch-via-ts-patch.mjs
@@ -1,48 +1,19 @@
 import '#pkg/polyfill-explicit-resource-management.mjs';
 
-import { findWorkspaceDir } from '@pnpm/find-workspace-dir';
-import { buildDependenciesHierarchy } from '@pnpm/reviewing.dependencies-hierarchy';
-import assert from 'node:assert';
+import * as commander from '@commander-js/extra-typings';
 import fs from 'node:fs';
-import path from 'node:path';
-import url from 'node:url';
 import { temporaryDirectory } from 'tempy';
 import { $ } from 'zx';
 
-const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-const monorepoRootDir = await findWorkspaceDir(__dirname);
-assert(monorepoRootDir);
-const workspaceDir = path.join(__dirname, '..');
-
-console.log(
-  'determining version of "typescript" we have currently in use by looking at the symlinked node_modules for this workspace project',
+const program = new commander.Command().addOption(
+  new commander.Option('--typescript-version <version>').makeOptionMandatory(),
 );
+program.parse();
+const opts = program.opts();
 
-const depsInfos = await buildDependenciesHierarchy([workspaceDir], {
-  lockfileDir: monorepoRootDir,
-  depth: 0,
-});
-
-/**
- * @type {string | undefined}
- */
-const typescriptVersion = depsInfos[workspaceDir]?.devDependencies?.find(
-  (dep) => dep.name === 'typescript',
-)?.version;
-if (!typescriptVersion) {
-  throw new Error(
-    `could not determine the version of "typescript" to patch, reason: could not find "typescript"!`,
-  );
-}
-
-console.log(`found typescript@${typescriptVersion}`);
-
-$.cwd = __dirname;
 await using typeScriptTempDir = createTempDir();
-await $`pnpm patch --edit-dir=${typeScriptTempDir.path} typescript@${typescriptVersion}`;
-await $`pnpm add ts-patch@3.0.2`;
-await $`pnpm exec ts-patch --dir=${typeScriptTempDir.path} install`;
-await $`pnpm rm ts-patch`;
+await $`pnpm patch --edit-dir=${typeScriptTempDir.path} typescript@${opts.typescriptVersion}`;
+await $`pnpm --package='typescript@${opts.typescriptVersion}' --package='ts-patch@3.0.2' dlx ts-patch --dir=${typeScriptTempDir.path} install`;
 await $`pnpm patch-commit ${typeScriptTempDir.path}`;
 
 function createTempDir() {
