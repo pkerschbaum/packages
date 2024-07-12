cdd6307543efa81c38e691cd6e0c9d5e5e3a3b06
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2024-07-12 23:10:38 +0200

    feat(pkg-management): add required CLI option `--ts-patch-version` for bin `create-pnpm-patch-via-ts-patch`

--- packages/pkg-management/src/create-pnpm-patch-via-ts-patch.mjs
+++ packages/pkg-management/src/create-pnpm-patch-via-ts-patch.mjs
@@ -5,15 +5,15 @@ import fs from 'node:fs';
 import { temporaryDirectory } from 'tempy';
 import { $ } from 'zx';
 
-const program = new commander.Command().addOption(
-  new commander.Option('--typescript-version <version>').makeOptionMandatory(),
-);
+const program = new commander.Command()
+  .addOption(new commander.Option('--typescript-version <version>').makeOptionMandatory())
+  .addOption(new commander.Option('--ts-patch-version <version>').makeOptionMandatory());
 program.parse();
 const opts = program.opts();
 
 await using typeScriptTempDir = createTempDir();
 await $`pnpm patch --edit-dir=${typeScriptTempDir.path} typescript@${opts.typescriptVersion}`;
-await $`pnpm --package='typescript@${opts.typescriptVersion}' --package='ts-patch@3.0.2' dlx ts-patch --dir=${typeScriptTempDir.path} install`;
+await $`pnpm --package='typescript@${opts.typescriptVersion}' --package='ts-patch@${opts.tsPatchVersion}' dlx ts-patch --dir=${typeScriptTempDir.path} install`;
 await $`pnpm patch-commit ${typeScriptTempDir.path}`;
 
 function createTempDir() {
