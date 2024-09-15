6bd7afca35b715c7611d9948f988602466f13901
Patrick Kerschbaum <patrick.kerschbaum@gmail.com>
2024-09-15 16:25:07 +0200

    chore: remove unused project

--- platform/config-nyc/nyc.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  extension: ['.ts', '.cts', '.mts', '.tsx', '.ctsx', '.mtsx'],
-  exclude: ['**/*.d.ts', '**/*.d.cts', '**/*.d.mts', '**/*.d.tsx', '**/*.d.ctsx', '**/*.d.mtsx'],
-  all: true,
-  excludeAfterRemap: 'false',
-  reporter: ['html', 'lcov'],
-};
